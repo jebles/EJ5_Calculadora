e37f853ef4b95a7dd32b0512937f6bcd83c84e4d
jebles <4004010@alu.murciaeduca.es>
2019-10-25 11:33:48 +0200

    pre_op

--- FormCalc.vb
+++ FormCalc.vb
@@ -1,10 +1,10 @@
 ﻿Public Class FormCalc
-    Dim ultimaoperacion As Char
+    Dim siguienteOp As Char
     Dim op0 As String
     Dim op1 As String
-    Dim op2 As String 'aqui se almacena el valor
     Dim opActivo As Boolean 'si false: operando 0 está activo, si true: operando 1 activo.
-    Dim resultado As Double
+    Dim resultado As Double = -1
+
 
     'NUMERICOS
     Private Sub Boton_num(sender As Object, e As EventArgs) Handles Btn8.Click, Btn7.Click, Btn9.Click, Btn6.Click, Btn5.Click, Btn4.Click, Btn3.Click, Btn2.Click, Btn1.Click, Btn0.Click
@@ -12,14 +12,10 @@
         boton = CType(sender, Button) 'recoge en un ojeto tipo boton las propiedades de cada boton numerico presionado
 
         If opActivo = 0 Then 'si esta activo el operando 0
-            If op0 = resultado Then 'caso especial cuando se pulsa un numero y el operando 0 guarda el resultao para op consecutivas
-                op0 = 0 'lo reinicia para operacion nueva
-            End If
             op0 = AddNum(boton.Text, op0)
         Else
             op1 = AddNum(boton.Text, op1)
         End If
-
     End Sub
 
     'BOTONES ESPECIALES
@@ -28,9 +24,7 @@
         If TxRes.Enabled = True Then
             TxRes.Clear()
             TxRes.Enabled = False
-
             TxRes.BackColor = Color.Black
-
             BtnOn.BackgroundImage = Image.FromFile(RutaRelativaA("off.png"))
             BtnOn.BackgroundImageLayout = ImageLayout.Zoom
             Reseteo()
@@ -52,7 +46,7 @@
         resultado = 0
         op0 = 0
         op1 = 0
-        ultimaoperacion = ""
+        siguienteOp = ""
         opActivo = False
         TxRes.Text = 0
     End Sub
@@ -66,84 +60,72 @@
         TxRes.Text = Convert.ToDouble(numContactenado)
         AddNum = numContactenado 'ver si se pone en boton_num luego
     End Function
+    Private Sub Operar(oper0 As String, oper1 As String)
 
-    Private Sub Operar(oper1 As String, oper2 As String)
-
-        Select Case ultimaoperacion
+        Select Case siguienteOp
             Case "+"
-                resultado = Convert.ToDouble(oper1) + Convert.ToDouble(oper2)
+                resultado = Convert.ToDouble(oper0) + Convert.ToDouble(oper1)
             Case "-"
-                resultado = Convert.ToDouble(oper1) - Convert.ToDouble(oper2)
+                resultado = Convert.ToDouble(oper0) - Convert.ToDouble(oper1)
             Case "*"
-                resultado = Convert.ToDouble(oper1) * Convert.ToDouble(oper2)
+                resultado = Convert.ToDouble(oper0) * Convert.ToDouble(oper1)
             Case "/"
-                resultado = Convert.ToDouble(oper1) / Convert.ToDouble(oper2)
+                resultado = Convert.ToDouble(oper0) / Convert.ToDouble(oper1)
         End Select
-
         TxRes.Text = resultado
     End Sub
 
     'OPERADORES 
     Private Sub BtnSuma_Click(sender As Object, e As EventArgs) Handles BtnSuma.Click
-        ultimaoperacion = "+"
+        siguienteOp = "+"
         comprobarOrdenOperandosYoperar()
     End Sub
-
     Private Sub comprobarOrdenOperandosYoperar()
         If opActivo = 0 Then 'si está activo el primer operando, activo el segundo para recibir datos
             opActivo = 1
-        Else 'si está activo el segundo operando, opero
-            Operar(op0, op1)
-            op0 = Convert.ToDouble(resultado)
-            op1 = 0
+        ElseIf opActivo = 1 Then 'si está activo el segundo operando, opero
+            BtnIgual.PerformClick()
         End If
         contIguales = 0
     End Sub
-
     Private Sub BtnResta_Click(sender As Object, e As EventArgs) Handles BtnResta.Click
-        ultimaoperacion = "-"
+        siguienteOp = "-"
         comprobarOrdenOperandosYoperar()
     End Sub
-
     Private Sub BtnMulti_Click(sender As Object, e As EventArgs) Handles BtnMulti.Click
-        ultimaoperacion = "*"
-        comprobarOrdenOperandosYoperar()
+        siguienteOp = "*"
+        'comprobarOrdenOperandosYoperar()
+        Operar(op0, op1)
     End Sub
-
     Private Sub BtnDiv_Click(sender As Object, e As EventArgs) Handles BtnDiv.Click
-        ultimaoperacion = "/"
-        comprobarOrdenOperandosYoperar()
+        siguienteOp = "/"
+        'comprobarOrdenOperandosYoperar()
+        Operar(op0, op1)
     End Sub
 
     Dim contIguales As Integer = 0
     Dim aux0 As String
     Dim aux1 As String
-    Dim operacionUnicaFinalizada As Boolean = False
     Private Sub BtnIgual_Click(sender As Object, e As EventArgs) Handles BtnIgual.Click
-
         '= consecutivo y primero (ops indepes)
         If contIguales = 0 Then 'primer igual 
             aux0 = op0
             aux1 = op1
-            MsgBox(aux0 & " " & ultimaoperacion & " " & aux1)
+            MsgBox(aux0 & " " & siguienteOp & " " & aux1)
             MsgBox("primer igual")
             Operar(op0, op1)
             opActivo = 0
-            'si se va a seguir sumando operandos
             op0 = resultado
-            'si se va a hacer otra operacion op0 = 
             op1 = 0
             contIguales += 1
-
         ElseIf contIguales = 1 Then '= operaciones consecutivas
             MsgBox("consecutivos")
-            MsgBox("aux0=" & aux0 & " " & ultimaoperacion & " aux1=" & aux1)
+            MsgBox("aux0=" & aux0 & " " & siguienteOp & " aux1=" & aux1)
             aux0 = resultado
             op0 = 0
             op1 = 0
             Operar(aux0, aux1)
         End If
-
     End Sub
     Public Function RutaRelativaA(nom As String)
         Dim quitarDePath As String = "bin\Debug"
@@ -158,5 +140,9 @@
         Else
             op1 = String.Concat("-", op1)
         End If
+    End Sub
+
+    Private Sub BtnDec_Click(sender As Object, e As EventArgs) Handles BtnDec.Click
+        'TxRes.Text = String.Concat()
     End Sub
 End Class
